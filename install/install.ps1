371d215c93655da62005573140bf765b0bca0815
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2026-01-18 14:26:21 -0600

    Added script that creates start menu shortcut in windows

--- install/install.ps1
+++ install/install.ps1
@@ -1,7 +1,32 @@
 param (
-    [string] $ExecutablePath
+    [string] $ExecutablePath = $(throw "Required parameter not specified: '-ExecutablePath'"),
     [string] $ShortcutName = "SimpleBackup",
-    [string] $Arguments,
-    [switch] $Admin
+    [string] $Arguments
 )
 
+$ErrorActionPreference = "Stop"
+
+$ScriptDir = $PSScriptRoot
+$ExecutablePath = (Resolve-Path $ExecutablePath).Path
+$StartMenuPath = Join-Path $env:USERPROFILE "Start Menu\Programs"
+$ShortcutPath = Join-Path $StartMenuPath "$ShortcutName`.lnk"
+
+# Icon
+$IconPath = Join-Path $ScriptDir 'simple-backup.ico'
+if (! (Test-Path $IconPath)) {
+    $IconPath = $null
+}
+
+if ( $IconPath ) {
+    $IconPath = (cp $IconPath (Split-Path $ExecutablePath) -PassThru).FullName
+}
+
+# Create Shortcut
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = $ExecutablePath
+if ($Arguments) { $Shortcut.Arguments = $Arguments }
+if ($IconPath) { $Shortcut.IconLocation = $IconPath }
+$Shortcut.Save()
+
+Write-Host "Created shortcut `"$ShortcutPath`"" -ForegroundColor Green
